847e398bffd5a2652139e2767057a53f1c48d82a
rommzz <rommy_zohara@yahoo.com>
2022-06-08 06:51:17 +0700

    setting post function button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,15 +12,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
-submit(){
+  String email = '';
+  String password = '';
 
-}
+  _submit() async {
+    try {
+      Response res;
+      var dio = Dio();
+      var data = {
+        'email': email,
+        'password': password,
+      };
+      res = await dio.post(
+          'https://smartlink-owner-0-gateway-fljnd6wana-de.a.run.app//auth/byemailsign',
+          data: data);
+      // ignore: avoid_print
+      print(data);
+    } catch (e) {
+      log('message');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yuan dodol',
+      title: 'Yuan dobol',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,31 +45,39 @@ submit(){
           title: const Text('Yuan dodol'),
         ),
         body: Column(
-          children:  [
-            const Center(
-              child: TextField(
-                decoration: InputDecoration(
-                  label: Text('Email'),
-                  hintText: 'email@email.com',
+          children: [
+            Center(
+              child: Column(children: [
+                Center(
+                  child: TextField(
+                    onChanged: ((String value) {
+                      email = value;
+                    }),
+                    decoration: const InputDecoration(
+                      label: Text('Email'),
+                      hintText: 'email@email.com',
+                    ),
+                  ),
                 ),
-                onChanged: ,
-              ),
-            ),
-            const Center(
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  label: Text('Password'),
-                  hintText: 'Kata Sandi',
+                Center(
+                  child: TextField(
+                    onChanged: (String value) {
+                      password = value;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      label: Text('Password'),
+                      hintText: 'Kata Sandi',
+                    ),
+                  ),
                 ),
-              ),
+              ]),
             ),
             Center(
-              child: TextButton(
-                onPressed: submit(),
-                child: const Text('sUbmit')
-                )
-            )
+                child: TextButton(
+              onPressed: (() => _submit()),
+              child: const Text('Submit'),
+            ))
           ],
         ),
       ),
